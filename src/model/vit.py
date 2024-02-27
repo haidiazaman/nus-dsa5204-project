a002971249cc8b74f25b0facc86214b458ead1ba
unknown <liawzhengkai@gmail.com>
2024-02-27 08:06:39 +0800

    Refactor code

--- src/model/vit.py
+++ src/model/vit.py
@@ -17,23 +17,24 @@ class ViT(nn.Module):
                  depth,
                  heads,
                  mlp_dim,
-                 pool = 'cls',
-                 channels = 3,
-                 dim_head = 64,
-                 dropout = 0.,
-                 emb_dropout = 0.):
+                 pool='cls',
+                 channels=3,
+                 dim_head=64,
+                 dropout=0.,
+                 emb_dropout=0.):
         super().__init__()
         image_height, image_width = pair(image_size)
         patch_height, patch_width = pair(patch_size)
 
-        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, \
+            'Image dimensions must be divisible by the patch size.'
 
         num_patches = (image_height // patch_height) * (image_width // patch_width)
         patch_dim = channels * patch_height * patch_width
         assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
 
         self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_height, p2 = patch_width),
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_height, p2=patch_width),
             nn.LayerNorm(patch_dim),
             nn.Linear(patch_dim, dim),
             nn.LayerNorm(dim),
