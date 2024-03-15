f7ee6e375ea4c24a27acf67df1e51e9463219e20
unknown <liawzhengkai@gmail.com>
2024-03-15 18:45:40 +0800

    Add TinyImageNet

--- src/dataset/imagenet.py
+++ src/dataset/imagenet.py
@@ -1,9 +1,8 @@
-import torch
 import torchvision
 import numpy as np
 
+from datasets import load_dataset
 from torch.utils.data import Dataset
-from torchvision import transforms
 from PIL import Image
 
 
@@ -20,4 +19,23 @@ class ImageNet(Dataset):
     def __getitem__(self, idx):
         img, label = self.dataset[idx]
         img = np.array(img)
+        return img, label
+
+
+class TinyImageNet(Dataset):
+    def __init__(self, split, transform=None):
+        self.split = split
+        self.transform = transform
+        self.dataset = load_dataset('zh-plus/tiny-imagenet')[split]
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, label = self.dataset[idx]['image'], self.dataset[idx]['label']
+        if np.shape(img) != (64, 64, 3):
+            img = np.dstack([img, img, img])
+            img = Image.fromarray(img)
+        if self.transform:
+            img = self.transform(img)
         return img, label
