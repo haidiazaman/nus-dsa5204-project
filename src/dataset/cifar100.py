0e659fbd146f87667b6abda06b2469d20c2246c8
unknown <liawzhengkai@gmail.com>
2024-03-16 17:51:17 +0800

    Initial commit

---
+++ src/dataset/cifar100.py
@@ -0,0 +1,20 @@
+import torchvision
+import numpy as np
+
+from torch.utils.data import Dataset
+
+
+class CIFAR100(Dataset):
+    def __init__(self, path, train=True, transform=None):
+        self.path = path
+        self.train = train
+        self.transform = transform
+        self.dataset = torchvision.datasets.CIFAR100(path, train=train, transform=transform)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        img, label = self.dataset[idx]
+        img = np.array(img)
+        return img, label
